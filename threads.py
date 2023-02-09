422834a6ebf80858645930f1eda0a3dc7fcc17fa
KAGrove <knut.a.grove@gmail.com>
2023-02-09 11:15:32 +0100

    Med threading, 2 enkle funksjoner

--- threads.py
+++ threads.py
@@ -8,5 +8,10 @@ def function2():
     for x in range(1000):
         print("TWO")
 
-function1()
-function2()
+
+t1 = threading.Thread(target=function1)
+
+t2 = threading.Thread(target=function2)
+
+t1.start()
+t2.start()
