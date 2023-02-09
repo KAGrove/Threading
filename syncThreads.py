b3bb9729a9df31fe59a970ea145ffb3f167751ed
KAGrove <knut.a.grove@gmail.com>
2023-02-09 13:54:42 +0100

    Lock, teller ned

--- syncThreads.py
+++ syncThreads.py
@@ -4,20 +4,27 @@ import time
 
 x = 8192
 
+lock = threading.Lock()
+
 def double():
-    global x
+    global x, lock
+    lock.acquire()
     while x < 16384:
         x *= 2
         print(x)
         time.sleep(1)
     print("Reached the maximum")
+    lock.release()
 
 def half():
     global x
+    lock.acquire()
     while x > 1:
         x /= 2
         print(x)
         time.sleep(1)
+    print("Reached the minimum")
+    lock.release()
 
 
 t1 = threading.Thread(target=half)
