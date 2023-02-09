18844ca291420d345dbf477ed4f829c8e8d7b13d
KAGrove <knut.a.grove@gmail.com>
2023-02-09 11:23:27 +0100

    Another text under

--- threads.py
+++ threads.py
@@ -1,11 +1,11 @@
 import threading
 
 def function1():
-    for x in range(1000):
+    for x in range(10):
         print("ONE")
 
 def function2():
-    for x in range(1000):
+    for x in range(10):
         print("TWO")
 
 
@@ -15,3 +15,7 @@ t2 = threading.Thread(target=function2)
 
 t1.start()
 t2.start()
+
+t1.join()	# Den under venter på at t1 er ferdig (ikke t2)
+		# Hm, t2 gjør seg også ferdig før teksten under
+print("Another text")
